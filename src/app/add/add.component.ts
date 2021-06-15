8029eb3ea5e2e449e2e927103ab87ac05985cf27
Jeremy <lav.jeremy22@gmail.com>
2021-06-15 15:12:57 +0200

    affichage json

---
+++ src/app/add/add.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.scss']
+})
+
+export class AddComponent implements OnInit, OnDestroy {
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    address: this.fb.group({
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', Validators.required]
+    })
+  });
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+  }
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    
+  }
+  ngOnDestroy(): void {
+    
+  }
+
+
+}
